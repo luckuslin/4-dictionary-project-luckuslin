e4fa8c8d3af7467d1326b155f983461a458b37f3
luckuslin <lukasl18@nycstudents.net>
2026-02-25 09:47:31 -0500

    f
    f

---
+++ test.py
@@ -0,0 +1,10 @@
+
+amazon=[
+    {"name": "SAMSUNG 32-Inch Class Full HD F6000 Smart TV" "price": 127.99,"catergory": "Electronics"},
+    {"name": "Xbox Series X 1TB Gaming Console Console", "price": 670, "catergory": "Electronics",},
+    {"name": "Dining Table Set for 4","price": 251.10,"catergory": "Home & Kitchen",},
+    {"name": "Bounty Quick Size Paper Towels, White, 8 Family Rolls","price": 24.42,"catergory": "Health & Household",},
+    {"name": "TERRO Ant Killer Bait Stations T300B","price": 11.97,"catergory": "Garden & Outdoor"},
+]
+    
+
